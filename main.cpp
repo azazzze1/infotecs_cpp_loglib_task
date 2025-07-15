49199017b9d34d6d365d47d55b21fea22b18eb36
azazzze1 <evshushkov@stud.eltech.ru>
2025-07-15 18:29:02 +0300

    Изменение Cmake для динамической сборки библиотеки

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 int main(int argc, char** argv)
 {
-	// Создаем логгер с уровнем INFO по умолчанию
     auto logger = LoggerFactory::createSocketLogger("127.0.0.1", 12345, LogLevel::INFO);
 
     if (!logger) {
@@ -12,12 +11,10 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    // Записываем логи разных уровней
     logger->log(LogLevel::INFO, "Это информационное сообщение");
     logger->log(LogLevel::WARNING, "Это предупреждение");
     logger->log(LogLevel::ERROR, "Это ошибка");
 
-    // Меняем уровень логирования на ERROR
     logger->setLogLevel(LogLevel::ERROR);
 
     logger->log(LogLevel::INFO, "[НЕ ДОЛЖЕН ПОЯВИТЬСЯ] Это сообщение уровня INFO (ниже ERROR)");

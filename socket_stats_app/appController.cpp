ea2a37a808f06633b47580ababfd7a99f7571d9e
azazzze1 <evshushkov@stud.eltech.ru>
2025-07-19 18:08:38 +0300

    +парсер лога; +appController; +подключение к сокету

---
+++ socket_stats_app/appController.cpp
@@ -0,0 +1,124 @@
+#include "appController.hpp"
+
+appController::appController(int argc, char* argv[]){
+    if(argc!= 5){
+        std::cerr<<"\tВведены не все параметры:"<<std::endl;
+        std::cout<<"\t"<<argv[0]<<" <socket-ip-adress> "<<"<socket-port> "<<"<N> "<<"<T>"<<std::endl;
+        return;
+    }
+
+    std::string socketAddress = argv[2];
+    int socketPort = std::stoi(argv[3]);
+
+    if (!connectToSocket(socketAddress, socketPort)){
+        std::cerr<<"\tЗапустите приложение с верными данными"<<std::endl; 
+        return;
+    }
+
+    N = std::stoi(argv[3]);
+    T = std::stoi(argv[4]);
+    flagForListenLoop = true;  
+}
+
+bool appController::connectToSocket(const std::string& socketAddress, int socketPort){
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd < 0){
+        std::cerr<<"\tОшибка при создании сокета"<<std::endl;
+        return false;
+    }
+
+    sockaddr_in serv_address{};
+    serv_address.sin_family = AF_INET;
+    serv_address.sin_port = htons(socketPort);
+
+    if (inet_pton(AF_INET, socketAddress.c_str(), &serv_address.sin_addr) <= 0){
+        std::cerr<<"\tНекорректный адрес"<<std::endl;
+        close(socketfd);
+        socketfd = -1;
+        return false;
+    }
+
+    if (connect(socketfd, (struct sockaddr *)&serv_address, sizeof(serv_address)) < 0){
+        std::cerr<<"\tОшибка при подключении к сокету"<<std::endl;
+        close(socketfd);
+        socketfd = -1;
+        return false; 
+    }
+
+    return true;
+}
+
+void appController::listenSocket(){
+    const int BUFFER_SIZE = 1024;
+    char buffer[BUFFER_SIZE];
+
+    while(flagForListenLoop){
+        memset(buffer, 0, BUFFER_SIZE);
+        int valread = read(socketfd, buffer, BUFFER_SIZE - 1);
+
+        if (valread<=0){
+            std::cerr<<"\tСоединение разоравно."<<std::endl;
+            flagForListenLoop = false; 
+            break; 
+        }
+
+        std::string receivedMessage(buffer);
+        std::istringstream iss(receivedMessage);
+        std::string line;
+
+        while(std::getline(iss,line)){
+            LogMessage logMessage;
+            if (parseLogMessage(line, logMessage)){
+                socketStatsCollector.addMessage(logMessage);
+            }
+        }
+    }
+}
+
+bool appController::parseLogMessage(const std::string& line, LogMessage& outLogMessage){
+    size_t dateStartPos = line.find('['); 
+    size_t dateEndPos = line.find(']');
+    size_t levelStartPos = line.find('[', dateStartPos+1);
+    size_t levelEndPos = line.find(']', levelStartPos+1);
+
+    if (dateStartPos == std::string::npos || dateEndPos != std::string::npos ||
+        levelStartPos == std::string::npos || levelEndPos == std::string::npos ||
+        levelEndPos + 2 >= line.length()){
+            std::cerr<<"\tНеправильный формат сообщения"<<std::endl;
+            return false;
+        }
+    
+    std::string strLogLevel = line.substr(levelStartPos+1, levelEndPos-levelStartPos-1);
+
+    std::optional<LogLevel> optLevel = LoggerUtils::stringToLevel(strLogLevel);
+    LogLevel logLevel;
+    if(LoggerUtils::validateLogLevel(optLevel, logLevel)){
+        outLogMessage.logLevel = logLevel; 
+    }else{
+        std::cerr<<"\tНекорректный уровень логирования!"<<std::endl;
+        return false;
+    }
+    
+    std::string logText = line.substr(levelEndPos + 2);
+    outLogMessage.text = logText;
+
+    std::string strLogDate = line.substr(dateStartPos+1, dateEndPos-dateStartPos-1);
+
+    std::tm tm = {};
+    std::istringstream ss(strLogDate);
+
+    ss >> std::get_time(&tm, "%a %b %d %H:%M:%S %Y");
+
+    if (ss.fail()) {
+        std::cerr<<"\tНекорректный формат даты в логе!"<<std::endl;
+        return false;
+    }
+
+    std::time_t time = std::mktime(&tm);
+    std::chrono::system_clock::time_point logDate = std::chrono::system_clock::from_time_t(time);
+
+    outLogMessage.date = logDate;
+
+    return true;
+}
+
--- socket_stats_app/appController.hpp
+++ socket_stats_app/appController.hpp
@@ -1,5 +1,28 @@
 #ifndef SOCKET_APP_CONTROLLER_HPP
 #define SOCKET_APP_CONTROLLER_HPP
 
+#include "socketStatsCollector.hpp"
+#include "loggerFactory.hpp"
+#include "loggerUtils.hpp"
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+
+class appController{
+public:
+    appController(int argc, char* argv[]);
+
+    bool connectToSocket(const std::string& socketAddress, int socketPort); 
+    void listenSocket();
+    bool parseLogMessage(const std::string& line, LogMessage& outLogMessage);
+
+
+private:
+    SocketStatsCollector socketStatsCollector;
+    bool flagForListenLoop = false;
+    size_t N;
+    size_t T;
+    int socketfd; 
+}; 
 
 #endif
